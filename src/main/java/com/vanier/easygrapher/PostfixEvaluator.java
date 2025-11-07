e864ebb9227ca9b4211668fbeca4bee14040e182
nepallium <122187419+nepallium@users.noreply.github.com>
2025-11-07 15:19:25 -0500

    Add basic postfix evaluator and infix parser

--- src/main/java/com/vanier/easygrapher/PostfixEvaluator.java
+++ src/main/java/com/vanier/easygrapher/PostfixEvaluator.java
@@ -4,12 +4,12 @@
  */
 package com.vanier.easygrapher;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
 /**
- *
  * @author Alex
  */
 public class PostfixEvaluator {
@@ -17,25 +17,21 @@ public class PostfixEvaluator {
     private final static String SUBTRACT = "-";
     private final static String MULTIPLY = "*";
     private final static String DIVIDE = "/";
-    
-    private Stack<Double> stack;
+    private final static String EXPONENT = "^";
 
-    public PostfixEvaluator() {
-        stack = new Stack<>();
-    }
-    
     public double evaluate(String[] expressionArr) {
-        Double operand1, operand2, result = 0.0;
-        
+        double operand1, operand2, result = 0.0;
+        Stack<Double> stack = new Stack<>();
+
         for (String token : expressionArr) {
             if (token == null) {
                 continue;
             }
-            
+
             if (isOperator(token)) {
                 operand2 = stack.pop();
                 operand1 = stack.pop();
-                
+
                 result = switch (token) {
                     case ADD -> operand1 + operand2;
                     case SUBTRACT -> operand1 - operand2;
@@ -43,11 +39,9 @@ public class PostfixEvaluator {
                     case DIVIDE -> operand1 / operand2;
                     default -> result;
                 };
-                
+
                 stack.push(result);
-            }
-            
-            else {
+            } else {
                 try {
                     Double num = Double.valueOf(token);
                     stack.push(num);
@@ -57,15 +51,109 @@ public class PostfixEvaluator {
                 }
             }
         }
-        
+
         return result;
     }
-    
-    public boolean isOperator(String c) {
+
+    public String[] convertInfix(String expr) {
+        if (expr == null || expr.isEmpty()) {
+            return null;
+        }
+
+        ArrayList<String> postfixList = new ArrayList<>();
+        Stack<String> operatorStack = new Stack<>();
+
+        for (int i = 0; i < expr.length(); i++) {
+            String currStr = String.valueOf(expr.charAt(i));
+            char currChr = currStr.charAt(0);
+
+            // if operand, add to postfix
+            if ((currChr >= 'a' && currChr <= 'z') ||
+                    (currChr >= 'A' && currChr <= 'Z') ||
+                    (currChr >= '0' && currChr <= '9'))
+                postfixList.add(currStr);
+
+            else if (currChr == '(') {
+                operatorStack.push(currStr);
+            }
+
+            // if closing bracket, pop everything until "("
+            else if (currChr == ')') {
+                while (!operatorStack.isEmpty() && !operatorStack.peek().equals("(")) {
+                    postfixList.add(operatorStack.pop());
+                }
+                operatorStack.pop();
+            } else if (isOperator(currStr)) {
+                while (!operatorStack.isEmpty() && !operatorStack.peek().equals("(") &&
+                        // previous operator cannot be greater priority
+                        (getOperatorPriority(operatorStack.peek()) > getOperatorPriority(currStr) ||
+                                // two operators of the same priority cannot stay tgt
+                                (getOperatorPriority(operatorStack.peek()) == getOperatorPriority(currStr) &&
+                                        !isRightAssociative(currStr)
+                                ))) {
+                    postfixList.add(operatorStack.pop());
+                }
+                operatorStack.push(currStr);
+            }
+        }
+
+        // pop remaining operators
+        while (!operatorStack.isEmpty()) {
+            postfixList.add(operatorStack.pop());
+        }
+
+        // catch exception and return as user input expr error
+
+        return postfixList.toArray(new String[0]);
+    }
+
+    /**
+     * Checks if the given string is a valid operator
+     *
+     * @param c the input String
+     * @return whether the string is a valid operator
+     */
+    private boolean isOperator(String c) {
         if (c == null) {
             return false;
         }
-        
-        return c.equals(ADD) || c.equals(MULTIPLY) || c.equals(SUBTRACT) || c.equals(DIVIDE);
+
+        return switch (c) {
+            case ADD, SUBTRACT, MULTIPLY, DIVIDE, EXPONENT,
+                 "(", ")" -> true;
+            default -> false;
+        };
+    }
+
+    /**
+     * Check if operator is right-associative
+     *
+     * @param operator the operator str
+     * @return whether the operator is right-associative
+     */
+    private boolean isRightAssociative(String operator) {
+        if (operator == null) {
+            return false;
+        }
+
+        return switch (operator) {
+            case EXPONENT, SUBTRACT -> true;
+            default -> false;
+        };
+    }
+
+    /**
+     * Returns precedence of operators
+     *
+     * @param operator operator str
+     * @return the precedence
+     */
+    private int getOperatorPriority(String operator) {
+        return switch (operator) {
+            case EXPONENT -> 3;
+            case MULTIPLY, DIVIDE -> 2;
+            case ADD, SUBTRACT -> 1;
+            default -> -1;
+        };
     }
 }
--- src/test/java/com/vanier/easygrapher/PostfixEvaluatorTest.java
+++ src/test/java/com/vanier/easygrapher/PostfixEvaluatorTest.java
@@ -4,6 +4,7 @@
  */
 package com.vanier.easygrapher;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,9 +31,59 @@ public class PostfixEvaluatorTest {
         String[] normal2 = {"4", "2", "+", "3", "5", "1", "-", "*", "+"};
         double result2 = pfEval.evaluate(normal2);
         assertEquals(18.0, result2, 0.001);
-        
+
         String[] normal3 = {"7", "4", "-3", "*", "1", "5", "+", "/", "*"};
         double result3 = pfEval.evaluate(normal3);
         assertEquals(-14.0, result3, 0.001);
+    }
+
+    @Test
+    public void testEvaluate_negativeFactor() {
+        String[] normal1 = {"-3", "*", "---4"};
+        double result1 = pfEval.evaluate(normal1);
+        assertEquals(12.0, result1, 0.001);
+
+        String[] normal2 = {"7", "4", "-3", "*", "1", "5", "+", "/", "*"};
+        double result2 = pfEval.evaluate(normal2);
+        assertEquals(-14.0, result2, 0.001);
+    }
+
+    @Test
+    public void testConvertInfix_regular() {
+        String normal1 = "((3+4)*2)/7";
+        String[] result1 = pfEval.convertInfix(normal1);
+        String[] expected1 =  {"3", "4", "+", "2", "*", "7", "/"};
+        assertArrayEquals(expected1, result1);
+
+        String normal2 = "(4+2)+(3*(5-1))";
+        String[] result2 = pfEval.convertInfix(normal2);
+        String[] expected2 = {"4", "2", "+", "3", "5", "1", "-", "*", "+"};
+        assertArrayEquals(expected2, result2);
+
+    }
+
+    @Test
+    public void testConvertInfix_negativeFactor() {
+        String normal1 = "-3*---4";
+        String[] result1 = pfEval.convertInfix(normal1);
+        String[] expected1 = {"-3", "*", "---4"};
+
+        String normal2 = "7*(4*-3)/(5+1)";
+        String[] result2 = pfEval.convertInfix(normal2);
+        String[] expected2 = {"7", "4", "-3", "*", "1", "5", "+", "/", "*"};
+        assertArrayEquals(expected2, result2);
+    }
+
+    @Test
+    public void testConvertInfix_emptySpace() {
+        String normal1 = "(  ( 3+4   )*2      )/7";
+        String[] result1 = pfEval.convertInfix(normal1);
+        String[] expected1 =  {"3", "4", "+", "2", "*", "7", "/"};
+        assertArrayEquals(expected1, result1);
+
+        String normal2 = "(4  + 2) +(3*(   5- 1))";
+        String[] result2 = pfEval.convertInfix(normal2);
+        String[] expected2 = {"4", "2", "+", "3", "5", "1", "-", "*", "+"};
+        assertArrayEquals(expected2, result2);
     }
 }
